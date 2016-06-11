88fc5f582ca5106017c4c3cc1f173db20ced239d
Shawn Scofield <shawnscof@gmail.com>
2016-06-10 20:39:55 -0500

    adding changes to allow index.js to accept an array of addon module functions, allowing it to play nicely with webpack environments

--- index.js
+++ index.js
@@ -7,9 +7,21 @@
 		if( addons instanceof Array ){
 
 			for( var addon = 0, length = addons.length; addon < length; addon++ ){
+				
+				var additive = addons[addon];
+				
+				if (additive instanceof Function) {
+					additive(THREE);
+				}
 
-				require("./addons/" + addons[addon] + ".js")(THREE);
-
+				else if (typeof additive === "string") {
+					require("./addons/" + additive + ".js")(THREE);
+				}
+				
+				else {
+					throw new Error("Invalid module type provided");
+				}
+				
 			};
 
 		};
